c0e491780c8eed78bc59c324d822cedc12fbef03
dhruvv01 <dhrubajit9038@gmail.com>
2021-11-25 19:03:02 +0530

    files updated

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-  
-
-count = 0
-let countEl=document.getElementById("count-el")
-function increment(){
-    console.log("the button was clicked") 
+let count = 0
+let countEl = document.getElementById("count-el")
+function increment() {
+    console.log("the button was clicked")
     count++
-    countEl.innerText=count
+    countEl.textContent = count
+
+}
+
+let saveEl = document.getElementById("save-el")
+function save() {
+
+    saveEl.textContent += count + " - "
+
+}
+
+function reset() {
+
+    count = 0
+    countEl.textContent = 0
+
 }
